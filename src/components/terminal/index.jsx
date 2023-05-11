277b3d9770a809eb793577f7aa6b3a993207a726
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-05-10 23:58:15 -0300

    typewritter terminal animation

--- src/components/terminal/index.jsx
+++ src/components/terminal/index.jsx
@@ -16,7 +16,26 @@ const Terminal = () => {
             </div>
         </div>
         <div className="terminal_body">
-            <Bash />
+            <div className="line line1">
+                <Bash />
+                <span className="typer typer1">Hi there! my name is francisco</span>
+            </div>
+            <div className="line line2">
+                <Bash />
+                <span className="typer typer2">I&apos;m a web developer from Bariloche, Argentina</span>
+            </div>
+            <div className="line line3">
+                <Bash />
+                <span className="typer typer3">I like to code, love to learn new technologies and build things on the web</span>
+            </div>
+            <div className="line line4">
+                <Bash />
+                <span className="typer typer4">I think the internet is for making our lives easier and should be accesible to everyone</span>
+            </div>
+            <div className="line line5">
+                <Bash />
+                <span className="typer typer">I&apos;m open to work or colaborate in new projects!</span>
+            </div>
         </div>
     </div>
   )
