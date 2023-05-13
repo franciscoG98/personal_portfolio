3e80e0de44e361ab07e397ee00bf26e2970b5f3c
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-05-13 14:13:14 -0300

    footer responsive

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,24 +7,24 @@ const Footer = () => {
   return (
     <footer className='footer'>
         <section>
-            <a href="https://www.linkedin.com/in/francisco-gonzalez-web-dev/" target="__blank" rel="noopener noreferrer" className="footer_item">
+            <a className="footer_item" href="https://www.linkedin.com/in/francisco-gonzalez-web-dev/" target="__blank" rel="noopener noreferrer">
                 <GrLinkedinOption size={60} />
                 <span>Linkedin</span>
             </a>
-            <a href="https://github.com/franciscoG98" target="__blank" rel="noopener noreferrer" className="footer_item">
+            <a className="footer_item" href="https://github.com/franciscoG98" target="__blank" rel="noopener noreferrer">
                 <SiGithub size={60} />
                 <span>Github</span>
             </a>
-            <a href="https://codepen.io/franciscog98" target="__blank" rel="noopener noreferrer" className="footer_item">
+            <a className="footer_item" href="https://codepen.io/franciscog98" target="__blank" rel="noopener noreferrer">
                 <SiCodepen size={60} />
                 <span>Codepen</span>
             </a>
-            <a href="mailto:franciscomanuel.gonzalez3@gmail.com" className="footer_item">
+            <a className="footer_item" href="mailto:franciscomanuel.gonzalez3@gmail.com">
                 <SiGmail size={60} />
                 <span>Mail</span>
             </a>
         </section>
-        <span>&copy;{new Date().getFullYear()} Francisco Gonzalez - All Rights Reserved</span>
+        <span className="copy">&copy;{new Date().getFullYear()} Francisco Gonzalez - All Rights Reserved</span>
     </footer>
   )
 }
